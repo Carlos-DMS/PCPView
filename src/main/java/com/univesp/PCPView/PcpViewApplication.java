c461cddee4267ba4178556c5f1003fc9785e084c
carlos <carlos.dsanguino@gmail>
2026-04-05 19:16:04 -0300

    config: estrutura inicial do projeto

---
+++ src/main/java/com/univesp/PCPView/PcpViewApplication.java
@@ -0,0 +1,13 @@
+package com.univesp.PCPView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univesp/PCPView/PcpViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.PCPView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcpViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
